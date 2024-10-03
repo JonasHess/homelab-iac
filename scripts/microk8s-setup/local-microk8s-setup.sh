0eaf2044ae7af196e3b895106d33fddbbab62a41
Jonas Hess <Jonas@Hess.pm>
2024-10-03 15:49:05 +0200

    install microk8s script

---
+++ scripts/microk8s-setup/local-microk8s-setup.sh
@@ -0,0 +1,133 @@
+#!/usr/bin/env bash
+set -e
+cd "$(dirname "$0")"
+
+list_environments() {
+  echo "Available environments:"
+  for dir in ../../environments/*/; do
+    [ -d "$dir" ] && echo "  - $(basename "$dir")"
+  done
+}
+
+if [ -z "$1" ]; then
+  echo "You are missing the environment argument!"
+  echo "Correct usage: $0 <environment> <kube-config-path>"
+  list_environments
+  exit 1
+fi
+
+if [ -z "$2" ]; then
+  echo "You are missing the kube-config-path argument!"
+  echo "Correct usage: $0 <environment> <kube-config-path>"
+  list_environments
+  exit 1
+fi
+
+ENVIRONMENT=$1
+KUBE_CONFIG_PATH=$2
+export ENVIRONMENT
+
+# Load parameters from the environment.yaml file
+ENV_FILE="../../environments/${ENVIRONMENT}/environment.yaml"
+if [ ! -f "$ENV_FILE" ]; then
+  echo "Error: Environment file $ENV_FILE not found!"
+  exit 1
+fi
+
+SERVER_IP=$(yq e '.server-ip' "$ENV_FILE")
+USERNAME=$(yq e '.username' "$ENV_FILE")
+IP_RANGE=$(yq e '.ip-range' "$ENV_FILE")
+CLUSTER_NAME=$(yq e '.cluster-name' "$ENV_FILE")
+
+show_help() {
+  echo "Usage: $0 <environment> <kube-config-path> [--help]"
+  echo
+  echo "Arguments:"
+  echo "  <environment>       The environment name."
+  echo "  <kube-config-path>  The path to the kube-config file."
+  echo "  --help              Display this help message."
+  exit 0
+}
+
+# Function to ask for user confirmation
+confirm() {
+  read -r -p "${1:-Are you sure? [y/N]} " response
+  case "$response" in
+    [yY][eE][sS]|[yY])
+      true
+      ;;
+    *)
+      false
+      ;;
+  esac
+}
+
+update_kube_config() {
+  local kube_config_path=$1
+  local new_cluster_name=$2
+  local new_user_name=$3
+  local new_context_name=$4
+
+  echo "Updating kube-config file at $kube_config_path..."
+
+  # Update cluster name
+  yq e -i ".clusters[].name = \"$new_cluster_name\"" "$kube_config_path"
+
+  # Update user name
+  yq e -i ".users[].name = \"$new_user_name\"" "$kube_config_path"
+
+  # Update context name
+  yq e -i ".contexts[].name = \"$new_context_name\"" "$kube_config_path"
+  yq e -i ".contexts[].context.cluster = \"$new_cluster_name\"" "$kube_config_path"
+  yq e -i ".contexts[].context.user = \"$new_user_name\"" "$kube_config_path"
+
+  # Update current-context
+  yq e -i ".\"current-context\" = \"$new_context_name\"" "$kube_config_path"
+
+  echo "kube-config file updated successfully!"
+}
+
+# Check if the help command is provided
+if [[ "$1" == "--help" ]]; then
+  show_help
+fi
+
+# Check if the kube-config file already exists
+if [ -f "$KUBE_CONFIG_PATH" ]; then
+  if confirm "The file $KUBE_CONFIG_PATH already exists. Do you want to replace it? [y/N]"; then
+    rm -f "$KUBE_CONFIG_PATH"
+  else
+    echo "Cannot proceed with the installation. Cannot replace $KUBE_CONFIG_PATH."
+    exit 1
+  fi
+fi
+
+echo "Connecting to the remote server via ssh://${USERNAME}@${SERVER_IP}..."
+# Create a random subdirectory in /tmp on the remote server
+REMOTE_TMP_DIR=$(ssh ${USERNAME}@${SERVER_IP} "mktemp -d /tmp/installMicrok8s.XXXXXX")
+
+echo "Copying script to the remote server into path ${REMOTE_TMP_DIR}..."
+scp ./remote-microk8s-setup.sh ${USERNAME}@${SERVER_IP}:${REMOTE_TMP_DIR}/remote-microk8s-setup.sh
+
+echo "Running the remote-microk8s-setup.sh script on the remote server..."
+ssh -t ${USERNAME}@${SERVER_IP} "sudo -S bash ${REMOTE_TMP_DIR}/remote-microk8s-setup.sh ${IP_RANGE}"
+
+# Copy the kube-config file to the specified path
+echo "Downloading kube-config file to the local machine..."
+scp ${USERNAME}@${SERVER_IP}:${REMOTE_TMP_DIR}/kube-config "$KUBE_CONFIG_PATH"
+echo "Download complete. The kube-config file is stored at $KUBE_CONFIG_PATH"
+
+# Rename cluster, user and context in kube-config
+update_kube_config "$KUBE_CONFIG_PATH" "$CLUSTER_NAME" "$CLUSTER_NAME" "$CLUSTER_NAME"
+
+echo "Setting the kube-config file permissions..."
+chmod 600 "$KUBE_CONFIG_PATH"
+
+# Clean up the remote server
+echo "Cleaning up the remote server..."
+ssh ${USERNAME}@${SERVER_IP} "rm -f ${REMOTE_TMP_DIR}/remote-microk8s-setup.sh ${REMOTE_TMP_DIR}/kube-config && rm ${REMOTE_TMP_DIR}/nohup.out && rmdir ${REMOTE_TMP_DIR}"
+
+echo "Checking the cluster nodes..."
+kubectl --kubeconfig="$KUBE_CONFIG_PATH" get nodes -o wide
+
+echo "$(tput setaf 2)Installation complete!$(tput sgr0)"
---
+++ scripts/microk8s-setup/remote-microk8s-setup.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+set -e
+
+cd "$(dirname "$0")"
+
+# Function to display the help message
+show_help() {
+  echo "Usage: $0 <ip-range> [--help]"
+  echo
+  echo "Arguments:"
+  echo "  <ip-range>  The IP range for MetalLB."
+  echo "  --help      Display this help message."
+  exit 0
+}
+
+# Function to ask for user confirmation
+confirm() {
+  read -r -p "${1:-Are you sure? [y/N]} " response
+  case "$response" in
+    [yY][eE][sS]|[yY])
+      true
+      ;;
+    *)
+      false
+      ;;
+  esac
+}
+
+# Check if the help command is provided
+if [[ "$1" == "--help" ]]; then
+  show_help
+fi
+
+# Check if the IP range parameter is provided
+if [ -z "$1" ]; then
+  echo "Error: IP range parameter is missing!"
+  echo "Usage: $0 <ip-range>"
+  exit 1
+fi
+
+IP_RANGE=$1
+
+
+ensure_microk8s_installed() {
+  echo "Checking if microk8s is already installed..."
+  if snap list | grep -q microk8s; then
+    echo "microk8s is already installed."
+    if confirm "Do you want to uninstall microk8s? [y/N]"; then
+      microk8s stop
+      snap remove microk8s
+      install_microk8s
+    else
+      echo "Skipping microk8s installation."
+    fi
+  else
+    install_microk8s
+  fi
+}
+
+install_microk8s() {
+  echo "Installing microk8s..."
+  snap install microk8s --classic
+  microk8s enable dns
+  microk8s enable metallb:$IP_RANGE
+
+  if confirm "Do you want to enable Nvidia GPU support? [y/N]"; then
+   microk8s enable nvidia
+  else
+    echo "Skipping GPU support."
+  fi
+
+  microk8s start
+  echo "microk8s installation complete!"
+}
+
+set_kubectl_config() {
+  echo "Refreshing certificates..."
+  microk8s refresh-certs --cert ca.crt
+
+  echo "Setting up kubeconfig..."
+  mkdir -p ~/.kube
+  if [ -f ~/.kube/config ]; then
+    if confirm "On the remote machine, fhe file ~/.kube/config already exists. Do you want to replace it? [y/N]"; then
+      microk8s config > ~/.kube/config
+    else
+      echo "Cannot proceed with the installation. Can not replace  ~/.kube/config."
+      exit 1
+    fi
+  else
+    microk8s config > ~/.kube/config
+  fi
+
+
+  echo "Waiting for microk8s to be ready..."
+  kubectl wait --for=condition=Ready nodes --all --timeout=600s
+
+  echo "copying kubeconfig to current directory"
+  cp ~/.kube/config ./kube-config
+  chmod 777 ./kube-config
+
+  # schedule task to delete kubeconfig after 3 minutes in the background
+   CURRENT_DIR=$(pwd)
+   nohup sh -c "(sleep 180 && rm ${CURRENT_DIR}/kube-config)" &
+
+
+  echo "microk8s setup complete!"
+}
+
+ensure_microk8s_installed
+set_kubectl_config
