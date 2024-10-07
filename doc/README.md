014d12185cbef4057d70fbad2014e8dc8a9a1985
Jonas Hess <Jonas@Hess.pm>
2024-10-07 23:26:35 +0200

    enable readarr

--- doc/README.md
+++ doc/README.md
@@ -7,6 +7,8 @@ sudo zpool create -m /mnt/tank1 tank1 raidz /dev/disk/by-id/ata-Samsung_SSD_870_
 ```bash
 zfs create -o dedup=on -o compression=on -o atime=off -o encryption=aes-256-gcm -o keyformat=passphrase tank1/encrypted
 zfs create -o dedup=on -o compression=on -o atime=off tank1/unencrypted
+zfs create -o dedup=on -o compression=zstd-3 -o atime=off tank0/unencrypted
+
 ```
 
 ## Creating snapshots
---
+++ scripts/directory-structure/media.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+BASE_DIR="/mnt/tank0/encrypted/media"
+
+declare -A DIRS_SUBDIRS
+DIRS_SUBDIRS=(
+  ["apps"]=""
+  ["audiobooks"]=""
+  ["books"]="comics ebooks magazines literature"
+  ["games"]=""
+  ["movies"]=""
+  ["music"]=""
+  ["nzb"]=""
+  ["_unknown"]="sonar radarr prowlarr manual"
+  ["tv-shows"]=""
+  ["tutorials"]=""
+)
+
+# Ensure the base directory is owned by root and has strict permissions
+echo "Setting up base directory with root ownership and permissions..."
+sudo mkdir -p "$BASE_DIR"
+sudo chown root:root "$BASE_DIR"
+sudo chmod 755 "$BASE_DIR"
+echo "Base directory '$BASE_DIR' created with root ownership and permissions set to 755."
+
+# Loop through the associative array to create directories and subdirectories
+for DIR in "${!DIRS_SUBDIRS[@]}"; do
+  sudo mkdir -p "$BASE_DIR/$DIR"
+  sudo chown root:root "$BASE_DIR/$DIR"
+
+  if [ -n "${DIRS_SUBDIRS[$DIR]}" ]; then
+    # Set permissions for the main directory (protected)
+    sudo chmod 755 "$BASE_DIR/$DIR"
+    echo "Directory '$BASE_DIR/$DIR' created with permissions set to 755 (protected)."
+
+    # Creating subdirectories with 777 permissions
+    echo "Creating subdirectories in '$DIR' with open read/write permissions..."
+    for SUBDIR in ${DIRS_SUBDIRS[$DIR]}; do
+      sudo mkdir -p "$BASE_DIR/$DIR/$SUBDIR"
+      sudo chown root:root "$BASE_DIR/$DIR/$SUBDIR"
+      sudo chmod 777 "$BASE_DIR/$DIR/$SUBDIR"
+      echo "Subdirectory '$BASE_DIR/$DIR/$SUBDIR' created with permissions set to 777."
+    done
+  else
+    # Set permissions for directories without subdirectories (open)
+    sudo chmod 777 "$BASE_DIR/$DIR"
+    echo "Directory '$BASE_DIR/$DIR' created with permissions set to 777."
+  fi
+done
+
+echo "All directories and subdirectories created with appropriate permissions."
